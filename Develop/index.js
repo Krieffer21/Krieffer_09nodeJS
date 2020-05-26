42238d5295671af62ce2763ee23e960f73b52d02
Kaelyn Rieffer <kaelynrieffer@gmail.com>
2020-05-26 15:51:38 -0700

    adding questions

--- Develop/index.js
+++ Develop/index.js
@@ -1,5 +1,58 @@
-const questions = [
+var inquirer = require("inquirer");
 
+
+const questions = [
+    {
+        type: "",
+        message: "",
+        name: "badge"
+      },
+     {
+        type: "",
+        message: "What is your project title?",
+        name: "title"
+      },
+      {
+        type: "",
+        message: "Please describe your project.",
+        name: "description"
+      },
+    //   {
+    //     type: "",
+    //     message: "",
+    //     name: "contents"
+    //   },
+      {
+        type: "",
+        message: "Please describe your installation procedures, including software requirments.",
+        name: "installation"
+      },
+      {
+        type: "",
+        message: "Please describe the usage of your project.",
+        name: "usage"
+      },
+      {
+        type: "",
+        message: "What is your license?",
+        name: "license"
+      },
+      {
+        type: "",
+        message: "Please list your contributions.",
+        name: "contribution"
+      },
+      {
+        type: "",
+        message: "",
+        name: "tests"
+      },
+      {
+        type: "",
+        message: "Please enter you GitHub email so that users can ask questions.",
+        name: "questions"
+      }
+ 
 ];
 
 function writeToFile(fileName, data) {
