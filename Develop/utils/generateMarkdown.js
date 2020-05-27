6b564404e9022d32e0b87f5395e7858dbbb8fc47
Kaelyn Rieffer <kaelynrieffer@gmail.com>
2020-05-27 14:38:40 -0700

    adding data names

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -2,7 +2,16 @@ function generateMarkdown(data) {
   return `
 # ${data.title}
 
+## ${data.description}
+
+## ${data.installation}
+## ${data.usage}
+## ${data.license}
+## ${data.contribution}
+## ${data.tests}
+## ${data.questions}
 `;
+
 }
 
 module.exports = generateMarkdown;
